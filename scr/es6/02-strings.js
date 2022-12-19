025891e8fda79b15defa151f562b880d9e0365b3
Luiggy102 <work_luiggyn.h5d9a@slmail.me>
2022-12-19 17:51:44 -0500

    added strings file

---
+++ scr/es6/02-strings.js
@@ -0,0 +1,16 @@
+let hello = 'hello';
+let world = 'world';
+
+let phrase = hello +  ' ' + world; // feo
+
+// tempale literal
+let phrase2 = `${hello} ${world}`;
+console.log(phrase);
+
+// multiline string
+
+let lorem = `
+Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+`;
+
+console.log(lorem);
